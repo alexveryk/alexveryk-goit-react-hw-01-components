8044a541867d7348d0fdd0afb4df9c6746f91e01
alexveryk <veryk.av@gmail.com>
2023-02-16 00:04:50 +0200

    Create Statistics.jsx

---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,28 @@
+import PropTypes from 'prop-types';
+
+export const Statistics = ({ title, stats }) => {
+  return (
+    <>
+      <div>{title.length > 0 && <h2>{title}</h2>}</div>
+      <ul className="stat-list">
+        {stats.map(({ label, percentage, id }) => (
+          <li key={id} className="item">
+            <span className="label">{label}</span>
+            <span className="percentage">{percentage}%</span>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+};
+
+Statistics.propTypes = {
+  title: PropTypes.string,
+  stats: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      label: PropTypes.string.isRequired,
+      percentage: PropTypes.number.isRequired,
+    })
+  ),
+};
