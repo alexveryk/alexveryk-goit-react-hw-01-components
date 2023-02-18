c0840cb98e3e7914f3ff1da818d764befad68515
alexveryk <veryk.av@gmail.com>
2023-02-18 23:13:21 +0200

    Create Statstics.styled.jsx

---
+++ src/components/Statistics/Statstics.styled.jsx
@@ -0,0 +1,32 @@
+import styled from '@emotion/styled';
+
+export const Container = styled.div`
+  width: 340px;
+  margin-bottom: 30px;
+`;
+export const Title = styled.div`
+  display: flex;
+  justify-content: center;
+  padding: 16px 8px;
+`;
+
+export const StatList = styled.ul`
+  display: flex;
+  list-style: none;
+  color: white;
+`;
+
+export const Item = styled.li`
+  display: flex;
+  flex-direction: column;
+  padding: 12px;
+  width: 44px;
+`;
+
+export const Label = styled.span`
+  padding: 4 px;
+`;
+
+export const Percentage = styled.span`
+  padding: 4px;
+`;
