85ec936aad1d671ffa7f5fde2ecfcfe456c958fb
alexveryk <veryk.av@gmail.com>
2023-02-18 13:36:07 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,11 @@
 import { Profile } from './Profile/Profile';
 import user from '../user.json';
 import data from '../data.json';
+import transactions from '../transactions.json';
+import friends from '../friends.json';
 import { Statistics } from './Statistics/Statistics';
+import { FriendList } from './FriendList/FriendList';
+import { TransactionHistory } from './TransactionHistory/TransactionHistory';
 
 export const App = () => {
   return (
@@ -14,6 +18,8 @@ export const App = () => {
         stats={user.stats}
       />
       <Statistics title="Upload stats" stats={data} />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
     </>
   );
 };
