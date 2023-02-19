b51929a6084a5631f4259ff84b92857cdb09fb2c
alexveryk <veryk.av@gmail.com>
2023-02-19 18:16:22 +0200

    Update FriendList.jsx

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,28 +1,12 @@
 import PropTypes from 'prop-types';
 import { List } from './FriendList.styled';
-import { Item } from './FriendList.styled';
-import { UserName } from './FriendList.styled';
-import { Status } from './FriendList.styled';
-import { isOnline } from './onlineStatus';
+import { UserCard } from './UserCard';
 
 export const FriendList = ({ friends }) => {
   return (
     <List>
       {friends.map(friend => {
-        return (
-          <Item key={friend.id}>
-            <Status style={{ backgroundColor: isOnline(friend.isOnline) }}>
-              {friend.isOnline}
-            </Status>
-            <img
-              className="avatar"
-              src={friend.avatar}
-              alt="User avatar"
-              width="48"
-            />
-            <UserName>{friend.name}</UserName>
-          </Item>
-        );
+        return <UserCard key={friend.id} list={friend} />;
       })}
     </List>
   );
@@ -31,10 +15,7 @@ export const FriendList = ({ friends }) => {
 FriendList.propTypes = {
   friends: PropTypes.arrayOf(
     PropTypes.shape({
-      avatar: PropTypes.string.isRequired,
-      name: PropTypes.string.isRequired,
-      isOnline: PropTypes.bool.isRequired,
       id: PropTypes.number.isRequired,
     })
-  ),
+  ).isRequired,
 };
